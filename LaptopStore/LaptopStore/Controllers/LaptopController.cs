a71204e6ec49f1d15d1d36c74f27fa9169cdb8db
boyfa <procfvu2012@gmail.com>
2019-04-09 21:14:20 +0700

    add new action to watch product of each type

--- LaptopStore/LaptopStore/Controllers/LaptopController.cs
+++ LaptopStore/LaptopStore/Controllers/LaptopController.cs
@@ -25,5 +25,55 @@ namespace LaptopStore.Controllers
             var NewLaptop = GetNewLaptop(48);
             return View(NewLaptop.ToPagedList(pageNum, pageSize));
         }
+
+        //laptop theo loai 
+        public ActionResult MacLaptop(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 0 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Acer(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 2 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult Asus(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 1 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Dell(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 3 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+        }
+
+        public ActionResult HP(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 5 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+
+        }
+
+        public ActionResult MSI(int? page)
+        {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
+            var l = from laptop in data.LAPTOPs where laptop.IDM == 4 select laptop;
+            return View(l.ToPagedList(pageNum, pageSize));
+
+        }
+
     }
 }
