b26fc5d8ac728d9b5fbcb71632ae61e43c95eaed
boyfa <procfvu2012@gmail.com>
2019-04-10 21:51:39 +0700

    add new ViewModel LaptopDetailsViewModel.cs

---
+++ LaptopStore/LaptopStore/Models/LaptopDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaptopStore.ViewModel
+{
+    public class LaptopDetailsViewModel
+    {
+        public int id { get; set; }
+        public string laptopName { get; set; }
+        public double laptopPrice { get; set; }
+        public string laptopImage { get; set; }
+        public string laptopScreen { get; set; }
+        public string laptopCpu { get; set; }
+        public string laptopRam { get; set; }
+        public string laptopVga { get; set; }
+        public string laptopOs { get; set; }
+        public float laptopQuantity { get; set; }
+    }
+}
