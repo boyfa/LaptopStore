226abd41dc16bbdf1245a1f77653520bc3506d0f
vanlonghutech <zzprozzphamvanlong@gmail.com>
2019-04-10 19:28:42 +0700

    add actionresult Laygiohang

---
+++ LaptopStore/LaptopStore/Controllers/BasketController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaptopStore.Controllers
+{
+    public class BasketController : Controller
+    {
+        // GET: Basket
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Basket
+        // GET: Basket
+
+        dbLaptopShopDataContext data = new dbLaptopShopDataContext();
+        public List<Basket> Laygiohang()
+        {
+            List<Basket> lstBasket = Session["Basket"] as List<Basket>;
+            if (lstBasket == null)
+            {
+
+                lstBasket = new List<Basket>();
+                Session["Basket"] = lstBasket;
+            }
+            return lstBasket;
+        }
+    }
+}
