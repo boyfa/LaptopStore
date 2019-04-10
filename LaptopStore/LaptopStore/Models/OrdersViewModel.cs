2d6aba5c1dcaa000c8d2afda069be03efa53b161
vinhnguyen080797 <makarogkon1997@gmail.com>
2019-04-10 20:05:52 +0700

    add new ViewModel OrdersViewModel.cs

---
+++ LaptopStore/LaptopStore/Models/OrdersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaptopStore.ViewModel
+{
+    public class OrdersViewModel
+    {
+        public int ido { get; set; }
+        public string TenkhachHang { get; set; }
+        public string SdtKhachHang { get; set; }
+        public DateTime NgayDat { get; set; }
+        public DateTime NgayGiao { get; set; }
+        public string TenNguoiNhan { get; set; }
+        public string DiaChi { get; set; }
+        public string DienThoai { get; set; }
+        public string TenSanPham { get; set; }
+
+        public int SoLuong { get; set; }
+        public double Gia { get; set; }
+
+
+
+    }
+}
